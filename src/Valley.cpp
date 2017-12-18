c03cd4e27e47e0e29c4a049fc5bbed9ddd8cc303
Dale Johnson <dale@Dales-MacBook-Pro.local>
2017-12-18 15:57:01 +0000

    Cleaned up the Valley.cpp file

--- src/Valley.cpp
+++ src/Valley.cpp
@@ -5,17 +5,11 @@ Plugin *plugin;
 
 void init(rack::Plugin *p) {
 	plugin = p;
-	// This is the unique identifier for your plugin
 	p->slug = "Valley Free";
 #ifdef VERSION
 	p->version = TOSTRING(VERSION);
 #endif
-	p->website = "https://github.com/VCVRack/Tutorial";
-	p->manual = "https://github.com/VCVRack/Tutorial/blob/master/README.md";
-
-	// For each module, specify the ModuleWidget subclass, manufacturer slug (for saving in patches), manufacturer human-readable name, module slug, and module name
+	p->website = "https://github.com/ValleyAudio/ValleyRackFree";
+	p->manual = "https://github.com/ValleyAudio/ValleyRackFree/blob/master/README.md";
 	p->addModel(createModel<TopographWidget>("Valley", "Topograph", "Topograph", SEQUENCER_TAG));
-
-	// Any other plugin initialization may go here.
-	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
 }
