086ae87d8d4555a1e769a06f1f5d06d884d96c90
Dale Johnson <dale@Dales-MacBook-Pro.local>
2018-03-19 15:49:26 +0000

    Fixed multiple range bug

--- src/Dexter/Dexter.cpp
+++ src/Dexter/Dexter.cpp
@@ -284,7 +284,7 @@ void Dexter::step() {
         opMultipleKnob[op] = (int)params[opParams[op][OP_MULT_PARAM]].value;
         multiple = opMultipleKnob[op] + (int)modMatrix[op].getDestinationValue(RATIO_DEST);
 
-        pOpFreqs[0] = freqLUT.getFrequency((opPitch[op]) + bPitch) * multiples[clampi(multiple, 0, 27)];
+        pOpFreqs[0] = freqLUT.getFrequency((opPitch[op]) + bPitch) * multiples[clampi(multiple, 0, 26)];
         __lowChordMask = __lowChordMaskTable[chordNotes.size() - 1];
         __highChordMask = __highChordMaskTable[chordNotes.size() - 1];
         __lowLeftGain = __lowLeftGainTable[chordNotes.size() - 1];
@@ -293,7 +293,7 @@ void Dexter::step() {
         __highRightGain = __highRightGainTable[chordNotes.size() - 1];
 
         for(unsigned long i = 0; i < chordNotes.size(); ++i) {
-            pOpFreqs[i + 1] = freqLUT.getFrequency((opPitch[op]) + aPitch + chordNotes[i]) * multiples[clampi(multiple, 0, 26)];
+            pOpFreqs[i + 1] = freqLUT.getFrequency((opPitch[op]) + aPitch + chordNotes[i]) * multiples[clampi(multiple, 0, 25)];
             if(opButtonLFO[op] > 0.f || masterLFO > 0.f) {
                 pOpFreqs[i + 1] /= 100.f;
             }
