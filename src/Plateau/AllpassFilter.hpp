8966a120ce55d467db8e8bbbd9ba8a615370b64f
Dale Johnson <dale@Dales-MacBook-Pro.local>
2018-06-19 00:30:02 +0100

    Allpass Filter is now templated

--- src/Plateau/AllpassFilter.hpp
+++ src/Plateau/AllpassFilter.hpp
@@ -1,18 +1,41 @@
 #pragma once
 #include "InterpDelay.hpp"
 
+template<class T>
 class AllpassFilter {
 public:
-    AllpassFilter();
-    AllpassFilter(long maxDelay, long initDelay, double gain);
-    double process();
-    void clear();
+    AllpassFilter() {
+        clear();
+        gain = 0;
+    }
 
-    double input;
-    double gain;
-    double output;
-    InterpDelay<double> delay;
+    AllpassFilter(long maxDelay, long initDelay, T gain) {
+        clear();
+        delay = InterpDelay<T>(maxDelay, initDelay);
+        this->gain = gain;
+    }
+
+    T inline process() {
+        _inSum = input + delay.output * gain;
+        output = delay.output + _inSum * gain * -1;
+        delay.input = _inSum;
+        delay.process();
+        return output;
+    }
+
+    void clear() {
+        input = 0;
+        output = 0;
+        _inSum = 0;
+        _outSum = 0;
+        delay.clear();
+    }
+
+    T input;
+    T gain;
+    T output;
+    InterpDelay<T> delay;
 private:
-    double _inSum;
-    double _outSum;
+    T _inSum;
+    T _outSum;
 };
