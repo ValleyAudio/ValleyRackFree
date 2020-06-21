0c6533eef47f007e9e55a9eb644a5d37121acc29
Dale Johnson <ds.johnson1992@gmail.com>
2020-06-21 02:07:01 +0100

    Add one shot trigger mode to AR envelope

--- src/Common/SIMD/VecAREnvelope.hpp
+++ src/Common/SIMD/VecAREnvelope.hpp
@@ -19,6 +19,7 @@ struct VecAREnvelope {
     __m128 riseFinished;
     __m128 targetValue;
     __m128 inputEpsilon, risingEpsilon, fallingEpsilon;
+    bool inOneShotMode;
 
     VecAREnvelope() {
         zeros = _mm_set1_ps(0.f);
@@ -36,15 +37,28 @@ struct VecAREnvelope {
         riseRate = _mm_set1_ps(0.99f);
         fallRate = _mm_set1_ps(0.99f);
         inputEpsilon = _mm_set1_ps(0.0001f);
-        risingEpsilon = _mm_set1_ps(0.002f);
+        risingEpsilon = _mm_set1_ps(0.998f);
         fallingEpsilon = _mm_set1_ps(0.000031f);
+        inOneShotMode = true;
     }
 
     __m128 process(const __m128& trigger) {
+        // Generate edge triggers that set start values when trigger changes
         risingEdge = _mm_cmpgt_ps(trigger, prevTrigger);
-        fallingEdge = _mm_cmplt_ps(trigger, prevTrigger);
-        rising = _mm_cmpgt_ps(trigger, zeros);
+        if (inOneShotMode) {
+            rising = _mm_switch_ps(rising, risingEdge, risingEdge);
+            fallingEdge = _mm_and_ps(rising, _mm_cmpgt_ps(env, risingEpsilon));
+            rising = _mm_switch_ps(rising, zeros, fallingEdge);
+        }
+        else {
+            fallingEdge = _mm_cmplt_ps(trigger, prevTrigger);
+            rising = _mm_cmpgt_ps(trigger, zeros);
+        }
+
+        falling = _mm_andnot_ps(rising, _mm_high_ps());
         targetValue = _mm_switch_ps(targetValue, trigger, risingEdge);
+
+        // Now set a start value when an edge is detected
         s.setStartValue(_mm_switch_ps(s.seg, _mm_sub_ps(targetValue, s.seg), risingEdge));
         s.setStartValue(_mm_switch_ps(s.seg, _mm_sub_ps(targetValue, s.seg), fallingEdge));
 
