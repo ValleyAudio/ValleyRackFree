3d097e19470fdf9f668ccca822c5852810ed6303
Dale Johnson <valley.audio.soft@gmail.com>
2020-07-12 22:23:13 +0100

    Add sine wave to sub osc, and shift phase of zero mode

--- src/Terrorform/TFormSubOsc.hpp
+++ src/Terrorform/TFormSubOsc.hpp
@@ -10,8 +10,10 @@ public:
     TFormSubOsc() {
         __zeros = _mm_set1_ps(0.f);
         __ones = _mm_set1_ps(1.f);
+        __twos = _mm_set1_ps(2.f);
         __halfs = _mm_set1_ps(0.5f);
         __negOnes = _mm_set1_ps(-1.f);
+        __pi = _mm_set1_ps(3.1415926f);
         reset();
     }
 
@@ -30,7 +32,12 @@ public:
         __b = _mm_sub_ps(__b, _mm_and_ps(__ones, _mm_cmpge_ps(__b, __ones)));
         __y = _mm_switch_ps(__negOnes, __ones, _mm_cmplt_ps(__a, __halfs));
         __y = _mm_add_ps(__y, _mm_polyblep_ps(__a, __stepSize));
-        return _mm_sub_ps(__y, _mm_polyblep_ps(__b, __stepSize));
+        __square = _mm_sub_ps(__y, _mm_polyblep_ps(__b, __stepSize));
+
+        __c = _mm_mul_ps(_mm_sub_ps(__a, __halfs), _mm_set1_ps(2.005f));
+        __sine = _mm_mul_ps(valley::_mm_sine_ps(_mm_mul_ps(__c, __pi)), __negOnes);
+
+        return __sine;
     }
 
     void reset() {
@@ -41,7 +48,8 @@ public:
     }
 
 private:
-    __m128 __zeros, __ones, __halfs, __negOnes;
-    __m128 __a, __b, __y;
+    __m128 __zeros, __ones, __twos, __halfs, __negOnes, __pi;
+    __m128 __a, __b, __c, __y;
     __m128 __counter, __stepSize;
+    __m128 __square, __sine;
 };
