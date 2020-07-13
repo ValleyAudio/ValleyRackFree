0f978cbeeba90940914dfd53dca01a048e9c2cb6
Dale Johnson <ds.johnson1992@gmail.com>
2020-07-14 00:35:15 +0100

    Abandon filtered sub, use 3 wave mix instead

--- src/Terrorform/TFormSubOsc.hpp
+++ src/Terrorform/TFormSubOsc.hpp
@@ -12,9 +12,7 @@ public:
         __zeros = _mm_set1_ps(0.f);
         __ones = _mm_set1_ps(1.f);
         __twos = _mm_set1_ps(2.f);
-        __fours = _mm_set1_ps(4.f);
         __halfs = _mm_set1_ps(0.5f);
-        __quarters = _mm_set1_ps(0.25f);
         __negOnes = _mm_set1_ps(-1.f);
         __pi = _mm_set1_ps(3.1415926f);
         reset();
@@ -34,24 +32,23 @@ public:
         __a = _mm_add_ps(__a, _mm_mul_ps(__counter, __halfs));
         __b = _mm_add_ps(__a, __halfs);
         __b = _mm_sub_ps(__b, _mm_and_ps(__ones, _mm_cmpge_ps(__b, __ones)));
+        __saw = _mm_sub_ps(_mm_mul_ps(__b, __twos), __ones);
+        __saw = _mm_sub_ps(__saw, _mm_polyblep_ps(__b, __stepSize));
+
         __y = _mm_switch_ps(__negOnes, __ones, _mm_cmplt_ps(__a, __halfs));
         __y = _mm_add_ps(__y, _mm_polyblep_ps(__a, __stepSize));
         __square = _mm_sub_ps(__y, _mm_polyblep_ps(__b, __stepSize));
-        __square = __filter.process(__square);
 
         __c = _mm_mul_ps(_mm_sub_ps(__a, __halfs), _mm_set1_ps(2.005f)); // Correct inaccuracy
         __sine = _mm_mul_ps(valley::_mm_sine_ps(_mm_mul_ps(__c, __pi)), __negOnes);
 
-        return _mm_linterp_ps(__sine, __square, __wave);
+        __output = _mm_linterp_ps(__sine, __saw, __wave1);
+        return _mm_linterp_ps(__output, __square, __wave2);
     }
 
     void setWave(const __m128& param) {
-        __param = param;
-        __wave = _mm_min_ps(__param, __quarters);
-        __wave = _mm_mul_ps(__wave, __fours);
-        __cutoff = _mm_max_ps(__param, __quarters);
-        __cutoff = _mm_sub_ps(__cutoff, _mm_set1_ps(0.2f));
-        __filter.setCutoffFreq(_mm_mul_ps(_mm_mul_ps(__cutoff, __cutoff), _mm_set1_ps(22050.f)));
+        __wave1 = _mm_min_ps(_mm_mul_ps(param, __twos), __ones);
+        __wave2 = _mm_clamp_ps(_mm_sub_ps(_mm_mul_ps(param, __twos), __ones), __zeros, __ones);
     }
 
     void setSampleRate(float sampleRate) {
@@ -66,10 +63,10 @@ public:
     }
 
 private:
-    __m128 __zeros, __ones, __twos, __fours, __halfs, __quarters, __tenths, __negOnes, __pi;
+    __m128 __zeros, __ones, __twos, __halfs, __negOnes, __pi;
     __m128 __a, __b, __c, __y;
     __m128 __counter, __stepSize;
-    __m128 __square, __sine, __output;
-    __m128 __param, __wave, __cutoff;
+    __m128 __square, __saw, __sine, __output;
+    __m128 __wave1, __wave2;
     VecOnePoleLPFilter __filter;
 };
