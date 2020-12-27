d7de1a262e1be0f1301fd8ce6327043d1db0a610
Dale Johnson <valley.audio.soft@gmail.com>
2020-12-27 00:58:33 +0000

    Correct non-linear processing glitch

--- src/Common/DSP/NonLinear.hpp
+++ src/Common/DSP/NonLinear.hpp
@@ -1,19 +1,21 @@
 #pragma once
 
+/** Tanh signal distortion using piecewise approximation of a hyperbolic tangent */
 inline float tanhDriveSignal(float x, float drive) {
     x *= drive;
 
-    if(x < -1.3f) {
+    if(x < -1.25f) {
         return -1.f;
     }
     else if(x < -0.75f) {
-        return (x * x + 2.6f * x + 1.69f) * 0.833333f - 1.f;
+        return 1.f - (x * (-2.5f - x) - 0.5625f) - 1.f;
     }
-    else if(x > 1.3f) {
+    else if(x > 1.25f) {
         return 1.f;
     }
     else if(x > 0.75f) {
-        return 1.f - (x * x - 2.6f * x + 1.69f) * 0.833333f;
+        return x * (2.5f - x) - 0.5625f;
     }
+
     return x;
 }
