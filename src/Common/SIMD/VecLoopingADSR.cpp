19a92a0edd14e774dfc7052ef5c9f465c27318a8
Dale Johnson <valley.audio.soft@gmail.com>
2021-01-01 23:29:21 +0000

    Add looping version of Fundamental ADSR

---
+++ src/Common/SIMD/VecLoopingADSR.cpp
@@ -0,0 +1,66 @@
+//
+// VecLoopingADSR.cpp
+// Author: Dale Johnson
+//
+// Copyright (c) 2020 Dale Johnson, Valley Audio
+// Licensed under GPLv3
+//
+
+#include "VecLoopingADSR.hpp"
+
+using namespace rack;
+using namespace rack::simd;
+
+void VecLoopingADSR::process(const float_4& gate, const float_4& trigger) {
+
+    // Gate and Looping
+    prevGated = gated;
+    gated = gate >= 1.f;
+    if (looping) {
+        gated = simd::ifelse(prevGated, float_4::mask(), gated);
+        attacking = simd::ifelse(sustaining, float_4::mask(), attacking);
+    }
+
+    // Retrigger
+    triggered = trigger >= 1.f;
+    attacking = simd::ifelse(triggered, float_4::mask(), attacking);
+
+    // Get target and lambda for exponential decay
+    const float attackTarget = 1.2f;
+    float_4 target = simd::ifelse(gated, simd::ifelse(attacking, attackTarget, sustain), 0.f);
+    float_4 lambda = simd::ifelse(gated, simd::ifelse(attacking, attackLambda, decayLambda), releaseLambda);
+
+    // Adjust env
+    env += (target - env) * lambda * sampleTime;
+
+    // Turn off attacking state if envelope is HIGH
+    attacking = simd::ifelse(env >= 1.f, float_4::zero(), attacking);
+
+    // Turn on attacking state if gate is LOW
+    attacking = simd::ifelse(gated, attacking, float_4::mask());
+
+    sustaining = (sustain <= env) & (env < sustain + EPSILON);
+}
+
+void VecLoopingADSR::setTimeScale(float timeScale) {
+    timeScale = clamp(timeScale, 0.f, 10.f);
+    minTimeScaled = MIN_TIME * timeScale;
+    maxTimeScaled = MAX_TIME * timeScale;
+    lamdaBase = maxTimeScaled / minTimeScaled;
+}
+
+void VecLoopingADSR::setSampleRate(float newSampleRate) {
+    sampleTime = 1.f / newSampleRate;
+}
+
+void VecLoopingADSR::setADSR(const float_4& attack, const float_4& decay,
+                             const float_4& sustain, const float_4& release) {
+    this->attack = simd::clamp(attack, 0.f, 1.f);
+    this->decay = simd::clamp(decay, 0.f, 1.f);
+    this->sustain = simd::clamp(sustain, 0.f, 1.f);
+    this->release = simd::clamp(release, 0.f, 1.f);
+
+    attackLambda = simd::pow(lamdaBase, -this->attack) / minTimeScaled;
+    decayLambda = simd::pow(lamdaBase, -this->decay) / minTimeScaled;
+    releaseLambda = simd::pow(lamdaBase, -this->release) / minTimeScaled;
+}
---
+++ src/Common/SIMD/VecLoopingADSR.hpp
@@ -0,0 +1,55 @@
+//
+// VecLoopingADSR.hpp
+// Author: Dale Johnson
+//
+// Copyright (c) 2020 Dale Johnson, Valley Audio
+// Licensed under GPLv3
+//
+// Based on the ADSR module from the VCV Fundamental plugin.
+// It adds a looping mode and timescale parameter.
+//
+
+#pragma once
+
+#include "rack.hpp"
+#include "simd/functions.hpp"
+
+using namespace rack::simd;
+
+class VecLoopingADSR {
+public:
+    float_4 attack = float_4::zero();
+    float_4 decay = float_4::zero();
+    float_4 sustain = float_4::zero();
+    float_4 release = float_4::zero();
+    float_4 env = float_4::zero();
+    float sampleTime = 1.f / 44100.f;
+
+    bool looping = false;
+
+    VecLoopingADSR() = default;
+
+    void process(const float_4& gate, const float_4& trigger);
+    void setTimeScale(float timeScale);
+    void setSampleRate(float newSampleRate);
+    void setADSR(const float_4& attack, const float_4& decay,
+                 const float_4& sustain, const float_4& release);
+
+private:
+    float MIN_TIME = 1e-3f;
+    float MAX_TIME = 10.f;
+    float lamdaBase = MAX_TIME / MIN_TIME;
+    float minTimeScaled = MAX_TIME;
+    float maxTimeScaled = MIN_TIME;
+    float EPSILON = 0.01f;
+
+    float_4 attacking = float_4::zero();
+    float_4 sustaining = float_4::zero();
+
+    float_4 attackLambda = float_4::zero();
+    float_4 decayLambda = float_4::zero();
+    float_4 releaseLambda = float_4::zero();
+    float_4 gated = float_4::zero();
+    float_4 prevGated = float_4::zero();
+    float_4 triggered = float_4::zero();
+};
