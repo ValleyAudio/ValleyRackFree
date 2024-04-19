95803c73b9df4237d1f7899fc382ea775b66a422
ValleyAudio <valley.audio.soft@gmail.com>
2024-04-20 00:07:39 +0100

    Remove leading single underscores from DOsc

--- src/dsp/generators/DOsc.hpp
+++ src/dsp/generators/DOsc.hpp
@@ -5,9 +5,9 @@
 
 class DOsc {
 public:
-    float _saw, _pulse, _pulsePhase;
-    float _subSaw, _subPulse;
-    float _pwm;
+    float saw, pulse, pulsePhase;
+    float subSaw, subPulse;
+    float pwm;
 
     enum Harmonic {
         SUB_2_OCTAVE_HARMONIC = 0,
@@ -21,79 +21,79 @@ public:
     };
 
     DOsc() {
-        _sampleRate = 44100.f;
-        _step = 0.f;
+        sampleRate = 44100.f;
+        step = 0.f;
         setFrequency(100.f);
         setSubOctave(0);
-        _subOffsetLevel = 0;
-        _subOffsetDegree = 0;
-        _subWidth = 0.5f;
-        _saw = 0.f;
-        _subSaw = 0.f;
-        _pwm = 0.5f;
+        subOffsetLevel = 0;
+        subOffsetDegree = 0;
+        subWidth = 0.5f;
+        saw = 0.f;
+        subSaw = 0.f;
+        pwm = 0.5f;
     }
 
     inline void process() {
         //// Fundamental Wave
-        _saw = (_step * 2.f - 1.f) - PolyBLEP(_step, _stepSize);
-        _pulse = _step + _pwm;
-        xInt = (int)_pulse;
-        _pulse -= (float)xInt;
-        _pulse = 1.f - _pulse;
-        _pulse = _saw + (_pulse * 2.f - 1.f) - PolyBLEP(_pulse, _stepSize);
-        _pulse = _pulse + (_pwm - 0.5f) * 2.f;
-        _saw = _sawHPF.process(_saw);
-        _pulse = _pulseHPF.process(_pulse);
+        saw = (step * 2.f - 1.f) - PolyBLEP(step, stepSize);
+        pulse = step + pwm;
+        xInt = (int)pulse;
+        pulse -= (float)xInt;
+        pulse = 1.f - pulse;
+        pulse = saw + (pulse * 2.f - 1.f) - PolyBLEP(pulse, stepSize);
+        pulse = pulse + (pwm - 0.5f) * 2.f;
+        saw = sawHPF.process(saw);
+        pulse = pulseHPF.process(pulse);
 
         //// Derive Sub Wave
-        _sub1Step = (_step * _subScale + (float)_subOffsetLevel * _subOffsetDegree);// + 0.75f;
-        xInt = (int)_sub1Step;
-        _sub1Step -= (float)xInt;
-        _subSaw = (_sub1Step * 2.f - 1.f) - PolyBLEP(_sub1Step, _subStepSize);
+        sub1Step = (step * subScale + (float)subOffsetLevel * subOffsetDegree);// + 0.75f;
+        xInt = (int)sub1Step;
+        sub1Step -= (float)xInt;
+        subSaw = (sub1Step * 2.f - 1.f) - PolyBLEP(sub1Step, subStepSize);
 
-        _subPulse = _sub1Step + _subWidth;
-        xInt = (int)_subPulse;
-        _subPulse -= (float)xInt;
-        _subPulse = 1.f - _subPulse;
-        _subPulse = _subSaw + (_subPulse * 2.f - 1.f) - PolyBLEP(_subPulse, _subStepSize);
-        _subPulse = _subPulse + (_subWidth - 0.5f) * 2.f;
-        _subPulse = _subHPF.process(_subPulse);
+        subPulse = sub1Step + subWidth;
+        xInt = (int)subPulse;
+        subPulse -= (float)xInt;
+        subPulse = 1.f - subPulse;
+        subPulse = subSaw + (subPulse * 2.f - 1.f) - PolyBLEP(subPulse, subStepSize);
+        subPulse = subPulse + (subWidth - 0.5f) * 2.f;
+        subPulse = subHPF.process(subPulse);
 
         // Increment phasor
-        _step += _stepSize;
-        if(_step > 1.f) {
-            _step -= 1.f;
-            _subOffsetLevel++;
-            _subOffsetLevel -= (_subOffsetLevel >= _subLimit) ? _subLimit : 0;
+        step += stepSize;
+        if(step > 1.f) {
+            step -= 1.f;
+            subOffsetLevel++;
+            subOffsetLevel -= (subOffsetLevel >= subLimit) ? subLimit : 0;
         }
     }
 
     inline void setFrequency(float f) {
-        _freq = f;
+        freq = f;
         _calcStepSize();
     }
 
     void setSampleRate(float sampleRate) {
-        _sampleRate = sampleRate;
+        sampleRate = sampleRate;
         _calcStepSize();
-        _sawHPF.setSampleRate(sampleRate);
-        _pulseHPF.setSampleRate(sampleRate);
-        _subHPF.setSampleRate(sampleRate);
+        sawHPF.setSampleRate(sampleRate);
+        pulseHPF.setSampleRate(sampleRate);
+        subHPF.setSampleRate(sampleRate);
     }
 
     void setSubWave(int subWave) {
         switch(subWave) {
             case 0:
-                _subWidth = 0.f;
+                subWidth = 0.f;
                 break;
             case 1:
-                _subWidth = 0.5;
+                subWidth = 0.5;
                 break;
             case 2:
-                _subWidth = 0.75f;
+                subWidth = 0.75f;
                 break;
             default:
-                _subWidth = 0.f;
+                subWidth = 0.f;
                 break;
         }
     }
@@ -101,66 +101,66 @@ public:
     void setSubOctave(int octave) {
         switch(octave) {
             case SUB_2_OCTAVE_HARMONIC:
-                _subScale = 0.25f;
-                _subLimit = 4;
-                _subOffsetDegree = 0.25f;
+                subScale = 0.25f;
+                subLimit = 4;
+                subOffsetDegree = 0.25f;
                 break;
             case SUB_OCTAVE_HARMONIC:
-                _subScale = 0.5f;
-                _subLimit = 2;
-                _subOffsetDegree = 0.5f;
+                subScale = 0.5f;
+                subLimit = 2;
+                subOffsetDegree = 0.5f;
                 break;
             case SUB_FIFTH_HARMONIC:
-                _subScale = 0.75f;
-                _subLimit = 4;
-                _subOffsetDegree = 0.75;
+                subScale = 0.75f;
+                subLimit = 4;
+                subOffsetDegree = 0.75;
                 break;
             case ZEROETH_HARMONIC:
-                _subScale = 1.f;
-                _subLimit = 1;
-                _subOffsetDegree = 0.f;
+                subScale = 1.f;
+                subLimit = 1;
+                subOffsetDegree = 0.f;
                 break;
             /*case THIRD_HARMONIC:
-                _subScale = 1.333333f;
-                _subWidth = 0.5f;
-                _subLimit = 3;
-                _subOffsetDegree = 0.333333f;
+                subScale = 1.333333f;
+                subWidth = 0.5f;
+                subLimit = 3;
+                subOffsetDegree = 0.333333f;
                 break;*/
             case FIFTH_HARMONIC:
-                _subScale = 1.5f;
-                _subLimit = 2;
-                _subOffsetDegree = 0.5f;
+                subScale = 1.5f;
+                subLimit = 2;
+                subOffsetDegree = 0.5f;
                 break;
             case OCTAVE_HARMONIC:
-                _subScale = 2.f;
-                _subLimit = 1;
-                _subOffsetDegree = 0.f;
+                subScale = 2.f;
+                subLimit = 1;
+                subOffsetDegree = 0.f;
                 break;
             case DOUBLE_OCTAVE_HARMONIC:
-                _subScale = 4.f;
-                _subLimit = 1;
-                _subOffsetDegree = 0.f;
+                subScale = 4.f;
+                subLimit = 1;
+                subOffsetDegree = 0.f;
                 break;
             default:
-                _subScale = 0.5f;
-                _subLimit = 2;
+                subScale = 0.5f;
+                subLimit = 2;
         }
-        _subStepSize = _stepSize * _subScale;
+        subStepSize = stepSize * subScale;
     }
 private:
 
-    float _freq, _sampleRate;
+    float freq, sampleRate;
 
-    float _stepSize, _subStepSize, _subWidth;
-    float _step, _sub1Step;
-    float _subScale, _subOffsetDegree;
-    int _subOffsetLevel, _subLimit;
+    float stepSize, subStepSize, subWidth;
+    float step, sub1Step;
+    float subScale, subOffsetDegree;
+    int subOffsetLevel, subLimit;
     int xInt;
-    DCBlocker _sawHPF, _pulseHPF, _subHPF;
+    DCBlocker sawHPF, pulseHPF, subHPF;
     void _calcStepSize() {
-        _stepSize = _freq / _sampleRate;
-        _stepSize = _stepSize > 0.5f ? 0.5f : _stepSize;
-        _subStepSize = _stepSize * _subScale;
+        stepSize = freq / sampleRate;
+        stepSize = stepSize > 0.5f ? 0.5f : stepSize;
+        subStepSize = stepSize * subScale;
     }
 
     inline float PolyBLEP(float t, float dt) {
