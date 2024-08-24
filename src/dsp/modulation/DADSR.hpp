07b5eff274fa4f4f895d529d10aca7924e3f1a17
ValleyAudio <valley.audio.soft@gmail.com>
2024-08-24 21:35:06 +0100

    In DADSR, combine envelope state flags into single enum class

--- src/dsp/modulation/DADSR.hpp
+++ src/dsp/modulation/DADSR.hpp
@@ -3,14 +3,17 @@
 
 class DEnv {
 public:
+    enum class State {
+        Idling,
+        Attacking,
+        Decaying,
+        Sustaining,
+        Releasing
+    };
+
     DEnv() {
-        _gated = false;
-        _triggered = false;
-        _idling = true;
-        _attacking = false;
-        _decaying = false;
-        _sustaining = false;
-        _releasing = false;
+        gated = false;
+        isTriggered = false;
         loop = false;
         value = 0.f;
 
@@ -27,77 +30,71 @@ public:
         setSampleRate(44100.f);
     }
     void process(float gate, float trig) {
-        if(trig >= 0.1f && _prevTrigState < 0.1f && !_triggered) {
-            _triggered = true;
+        if(trig >= 0.1f && _prevTrigState < 0.1f && !isTriggered) {
+            isTriggered = true;
         }
         _prevTrigState = trig;
 
-        _prevGateState = _gated;
-        _gated = gate >= 0.1f ? true : false;
+        _prevGateState = gated;
+        gated = gate >= 0.1f ? true : false;
 
-        if(_gated || _triggered) {
-            if(_idling || _releasing) {
-                _triggered = false;
-                _attacking = true;
-                _idling = false;
-                _decaying = false;
-                _sustaining = false;
-                _releasing = false;
+        if(gated || isTriggered) {
+            if(state == State::Idling ||
+               state == State::Releasing) {
+                isTriggered = false;
+                state = State::Attacking;
             }
         }
 
-        if(!_gated && _prevGateState) {
-            if(!_idling) {
-                _attacking = false;
-                _decaying = false;
-                _sustaining = false;
-                _releasing = true;
+        if(!gated && _prevGateState) {
+            if(state != State::Idling) {
+                state = State::Releasing;
             }
         }
 
         // The envelope
-        if(_idling) {
+        if(state == State::Idling) {
             value = 0.f;
         }
-        if(_attacking) {
+
+        if(state == State::Attacking) {
             value += std::powf(base, 1 - attackTime) / maxTime * (1.01f - value) * _sampleTime * timeScale;
             if(value > (1.f - 1e-4)) {
                 value = 1.f;
-                _attacking = false;
-                if(_gated) {
-                    _decaying = true;
+                if(gated) {
+                    state = State::Decaying;
                 }
                 else {
-                    _releasing = true;
+                    state = State::Releasing;
                 }
-            }
-        }
-        if(_decaying) {
-            value += std::powf(base, 1 - decayTime) / maxTime * (sustain - value) * _sampleTime * timeScale;
-            if(value < sustain + 1e-4) {
-                value = sustain;
-                _decaying = false;
-                _sustaining = true;
-            }
-        }
-        if(loop) {
-            if(_sustaining || _releasing) {
-                value = 0.f;
-                _attacking = true;
-                _sustaining = false;
-                _releasing = false;
             }
         }
 
-        if(_sustaining) {
+        if(state == State::Decaying) {
+            value += std::powf(base, 1 - decayTime) / maxTime * (sustain - value) * _sampleTime * timeScale;
+            if(value < sustain + 1e-4) {
+                value = sustain;
+                state = State::Sustaining;
+            }
+        }
+
+        if(loop) {
+            if(state == State::Sustaining ||
+               state == State::Releasing) {
+                value = 0.f;
+                state = State::Attacking;
+            }
+        }
+
+        if(state == State::Sustaining) {
             value = sustain;
         }
-        if(_releasing) {
+
+        if(state == State::Releasing) {
             value += std::powf(base, 1 - releaseTime) / maxTime * (0.0f - value) * _sampleTime * timeScale;
             if(value < 1e-4) {
                 value = 0.f;
-                _releasing = false;
-                _idling = true;
+                state = State::Idling;
             }
         }
     }
@@ -119,7 +116,8 @@ private:
     float base, maxTime;
     bool _prevGateState;
     float _prevTrigState;
+    State state = State::Idling;
     bool _idling, _attacking, _decaying, _sustaining, _releasing;
-    bool _gated, _triggered;
+    bool gated, isTriggered;
 
 };
